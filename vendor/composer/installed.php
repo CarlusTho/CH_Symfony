01c6edc43892fde06bad4c9894e111cf64e3b8e9
tcar <thomasulrac@gmail.com>
2021-11-17 15:00:09 +0100

    Ptite update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b4cf9c0d45962c4187364bfcb0753b60102325e8',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b4cf9c0d45962c4187364bfcb0753b60102325e8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
